ee08b284f4fa303a43903d9d5a5ff0dd9207c48f
gavin2059 <gavin2600@gmail.com>
2020-12-31 15:41:12 +0800

    Basic site complete. Need to fill in sections & fix email form (?)

---
+++ forms/contact.php
@@ -0,0 +1,20 @@
+<?php
+
+  $receiving_email_address = 'zz66@rice.edu';
+  
+  include( $php_email_form );
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
